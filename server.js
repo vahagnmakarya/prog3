ad26fc136bd5a013a1dac95ca2fc5566f4240406
vahagnmakarya <vahagn.makaryan.y@tumo.org>
2023-02-14 10:49:51 +0300

    3.1

--- server.js
+++ server.js
@@ -1,28 +1,3 @@
-// var express = require("express");
-// var app = express();
-
-// app.get("/", function (req, res) {
-//     res.send("Hello world");
-// });
-
-// app.listen(3000, function () {
-//     console.log("Example is running on port 3000");
-// });
-
-// var express = require("express");
-// var app = express();
-
-// app.get("/", function(req, res){
-//    res.send("<h1>Hello world</h1>");
-// });
-// app.get("/name/:name", function(req, res){
-//    var name = req.params.name;
-//    res.send("<h1>Hello " + name +"</h1>");
-// });
-// app.listen(3000, function(){
-//    console.log("Example is running on port 3000");
-// });	
-
 var express = require("express");
 var app = express();
 
@@ -36,3 +11,17 @@ app.listen(3000, function(){
    console.log("Example is running on port 3000");
 });
 
+// var express = require("express");
+// var app = express();
+
+
+// app.get("/", function(req, res){
+//    res.redirect('https://google.com');
+// });
+// app.get("/*", function(req, res){
+//    res.redirect('https://www.hostinger.com/tutorials/how-to-fix-error-404');
+// });
+
+// app.listen(3000, function(){
+//    console.log("Example is running on port 3000");
+// });
